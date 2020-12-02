6d1e433ed4fdea91ed85cc19f5120d6cf9c5f880
sbhusal123 <suryabhusal11@gmail.com>
2020-12-02 13:43:05 +0545

    Example of useMemo hooks.

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 function Counter() {
     const [counterOne, setCounterOne] = useState(0);
@@ -12,11 +12,23 @@ function Counter() {
         setCounterTwo(counterTwo + 1);
     };
 
+    const isEven = useMemo(() => {
+        let i = 0;
+        // Simulating a delyed computation
+        while (i < 200000000) {
+            i++;
+        }
+        return counterOne % 2 === 0;
+    }, [counterOne]);
+
+    // Use Memo is executed when on of the dependency has changes. Used to reduce expensive calculation when component rerenders
+
     return (
         <div>
             <div>
                 <button onClick={increamentOne}>
                     CounterOne -{counterOne}
+                    <span>{isEven ? "Even" : "Odd"}</span>
                 </button>
             </div>
             <div>
